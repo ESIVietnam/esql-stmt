794694c028bd7976dc577f1e669e69e00055be58
Pham Huy <pham.huy.19951126@gmail.com>
2022-02-28 09:32:29 +0700

    Add ValueTimestampTest

--- esql-data/src/test/java/esql/data/ValueDateTimeTest.java
+++ esql-data/src/test/java/esql/data/ValueDateTimeTest.java
@@ -2,6 +2,7 @@ package esql.data;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.nio.charset.StandardCharsets;
 import java.time.Instant;
@@ -118,15 +119,50 @@ public class ValueDateTimeTest {
 		/*Test
 		 * convertTo() function
 		 * */
-		
+		LocalDateTime value = (LocalDateTime) valueLocalDateTime.toTemporal();
 		assertEquals(valueLocalDateTime.convertTo(Types.TYPE_DATE),ValueDateTime.buildDateTime(((LocalDateTime) valueLocalDateTime.toTemporal()).toLocalDate()));
 		assertEquals(valueLocalDateTime.convertTo(Types.TYPE_DATETIME),valueLocalDateTime);
 		assertEquals(valueLocalDateTime.convertTo(Types.TYPE_TIMESTAMP),ValueDateTime.buildDateTime(((LocalDateTime) valueLocalDateTime.toTemporal()).atZone(ZoneId.systemDefault())));
-//		assertEquals(valueLocalDateTime.convertTo(Types.TYPE_INT), ValueNumber.buildNumber(Types.TYPE_INT, (((LocalDateTime) valueLocalDateTime.toTemporal()).toEpochSecond(ZoneOffset.of(ZoneId.systemDefault().getId())) * 1000)));
+		assertEquals(valueLocalDateTime.convertTo(Types.TYPE_INT), ValueNumber.buildNumber(Types.TYPE_INT, value.toEpochSecond(ZoneId.systemDefault().getRules().getOffset(value)) * 1000) );
 		assertEquals(valueLocalDateTime.convertTo(Types.TYPE_STRING),ValueString.buildString(Types.TYPE_STRING, valueLocalDateTime.stringValue()));
-		
-		System.out.println(valueLocalDateTime.convertTo(Types.TYPE_STRING));
-		System.out.println(ValueString.buildString(Types.TYPE_STRING, valueLocalDateTime.stringValue()));
+		assertEquals(valueLocalDateTime.convertTo(Types.TYPE_BYTES), ValueBytes.buildBytes(value.toString().getBytes(StandardCharsets.ISO_8859_1)));
+
+	}
 	
+	@Test
+	public void valueTimestampTest() {
+		ValueTimestamp valueTimestamp = new ValueTimestamp(Instant.now());
+		ValueTimestamp valueTimestamp2 = new ValueTimestamp(Instant.now());
+		
+		Value valueTime = new ValueLocalTime(LocalTime.now());
+		
+		ValueLocalDate valueLocalDate = new ValueLocalDate(LocalDate.of(2020, 11, 20));
+		
+		ValueLocalDateTime valueLocalDateTime = new ValueLocalDateTime(LocalDateTime.of(2020,11, 28, 12, 35));
+
+		ValueTimestampWithTZ valueTimestampWithTZ = new ValueTimestampWithTZ(ZonedDateTime.of(LocalDateTime.now(), ZoneId.of("Asia/Ho_Chi_Minh")));
+		
+		Value valueNumber = new ValueNumberLong(5000l);
+		/*Test
+		 * comparedTo() function
+		 * 
+		 * */
+		assertThrows(IllegalArgumentException.class, new Executable() {
+
+			@Override
+			public void execute() throws Throwable {
+				valueTimestamp.compareTo(valueTime);
+			}
+		});
+		
+		assertEquals(valueTimestamp.compareTo(valueTimestamp2),0);
+		assertEquals(valueTimestamp.compareTo(valueLocalDate), 1);
+		assertEquals(valueTimestamp.compareTo(valueLocalDateTime),1);
+		assertEquals(valueTimestamp.compareTo(valueTimestampWithTZ),0);
+		
+		assertEquals(null, null);
+		
+		
+		
 	}
 }
