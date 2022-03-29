367277d0eced0805bf3687a612cae0f7defb5436
Pham Huy <pham.huy.19951126@gmail.com>
2022-03-29 09:07:23 +0700

    Add DataTreeTest Level 2 and 3

--- esql-data/src/test/java/esql/data/DataTreeTest.java
+++ esql-data/src/test/java/esql/data/DataTreeTest.java
@@ -2,15 +2,22 @@ package esql.data;
 
 import org.junit.jupiter.api.Test;
 
+import esql.data.ValueDataTree.TreeBuilder;
+
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.StringWriter;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,10 +32,14 @@ public class DataTreeTest {
         assertTrue(data_tree.isEmpty(),"must be empty");
         assertEquals(0, data_tree.size());
         assertEquals("", data_tree.stringValue());
+        
+        
 
         try {
             ByteArrayOutputStream bo = new ByteArrayOutputStream(1024);
             data_tree.writeAsJSON(bo);
+            
+           
             assertEquals(0, bo.size()); //only {}
         } catch (IOException e) {
             fail(e.toString());
@@ -55,7 +66,10 @@ public class DataTreeTest {
         assertTrue(data_tree.isEmpty(),"must be empty");
         assertEquals(0, data_tree.size());
         assertEquals("({})", data_tree.stringValue());
-
+        
+        System.out.println(data_tree.stringValue());
+        
+        
         try {
             ByteArrayOutputStream bo = new ByteArrayOutputStream(1024);
             data_tree.writeAsJSON(bo);
@@ -111,12 +125,15 @@ public class DataTreeTest {
     public void OneLevelTreeTest() {
         ValueDataTree.TreeBuilder tb = ValueDataTree.builderOfDataTree();
         tb.putElement(Key.of("n"), 1);
+        
+        
         tb.putElement(Key.of("str"), "value");
         tb.putElement(Key.of("dou"), 4.5d);
         tb.putElement(Key.of("b"), true);
         tb.putNull(Key.of("nu"));
         tb.putElement(Key.of("nu2"), tb.nullElement());
         tb.putElement(Key.of("big_dec"), BigDecimal.TEN);
+        
         tb.putElement(Key.of("big_neg"), BigDecimal.valueOf(-1298283729L));
         tb.putElement(Key.of("bigint"), new BigInteger("2888765432"));
         byte[] a = { 0x01, 0x45, (byte) 0xa3 };
@@ -137,6 +154,8 @@ public class DataTreeTest {
             data_tree.writeAsJSON(bo);
             JsonReader reader = Json.createReader(new ByteArrayInputStream(bo.toByteArray()));
             JsonObject obj = reader.readObject();
+            
+            
             assertEquals("value", obj.getString("str"));
             assertEquals(1, obj.getInt("n"));
             assertEquals(4.5d, obj.getJsonNumber("dou").doubleValue());
@@ -161,4 +180,169 @@ public class DataTreeTest {
             fail(e.toString());
         }
     }
+    
+    @Test
+    public void TwoLevelTreeTest() throws IOException {
+    	ValueDataTree.TreeBuilder tb = ValueDataTree.builderOfDataTree();
+    	
+    	 tb.putElement(Key.of("str"), "value");
+         tb.putElement(Key.of("dou"), 4.5d);
+         tb.putElement(Key.of("b"), true);
+         tb.putNull(Key.of("nu"));
+         tb.putElement(Key.of("nu2"), tb.nullElement());
+         tb.putElement(Key.of("big_dec"), BigDecimal.TEN);
+     
+         tb.putElement(Key.of("big_neg"), BigDecimal.valueOf(-1298283729L));
+         tb.putElement(Key.of("bigint"), new BigInteger("2888765432"));
+         byte[] a = { 0x01, 0x45, (byte) 0xa3 };
+         tb.putElement(Key.of("abc"), a);
+         
+         
+         tb.putElement(Key.of("str2"), "value");
+         tb.putElement(Key.of("dou2"), 4.5d);
+         tb.putElement(Key.of("b2"), true);
+         tb.putNull(Key.of("nu2"));
+         tb.putElement(Key.of("nu3"), tb.nullElement());
+         tb.putElement(Key.of("big_dec2"), BigDecimal.TEN);
+         
+         tb.putElement(Key.of("big_neg2"), BigDecimal.valueOf(-1298283729L));
+         tb.putElement(Key.of("bigint2"), new BigInteger("2888765432"));
+         byte[] a2 = { 0x01, 0x45, (byte) 0xa3 };
+         tb.putElement(Key.of("abc2"), a);
+         
+         ValueDataTree.ListBuilder listBuilder = tb.createList(Key.of("SubList"));
+     	
+     	
+     	listBuilder.addElement(new StringElement("Value of SubList"),
+     			new NumberElement(Types.TYPE_INT, 27),
+     			new NumberElement(Types.TYPE_LONG, 9l),
+     			new NumberElement(Types.TYPE_DOUBLE, 9.5d),
+     			new NumberElement(Types.TYPE_DECIMAL, BigDecimal.valueOf(-12312894612L)),
+     			new NumberElement(Types.TYPE_ULONG, new BigInteger("1231259787813"))
+     			);
+     	
+     	
+     			
+     		
+    	ValueDataTree data_tree = tb.build();
+    	
+    	
+    	OutputStream outputStream = new ByteArrayOutputStream();
+        data_tree.writeAsJSON(outputStream);
+        
+        JsonReader reader = Json.createReader(new ByteArrayInputStream(((ByteArrayOutputStream) outputStream).toByteArray()));
+        
+        JsonObject jsonObject = reader.readObject();
+        
+        System.out.println("------------Level 2 Data Tree Test JSON-------------");
+        System.out.println(jsonObject);
+        System.out.println("-----------------------------------------------------");
+        
+        assertEquals("value", jsonObject.getString("str2"));
+        assertEquals(4.5d, jsonObject.getJsonNumber("dou").doubleValue());
+        assertEquals(new BigInteger("2888765432"), jsonObject.getJsonNumber("bigint2").bigIntegerValue());
+        
+        
+        ByteArrayOutputStream bo = new ByteArrayOutputStream(1024);
+        data_tree.writeAsXML("opt",bo);
+        String v = bo.toString(StandardCharsets.UTF_8);
+        System.out.println("------------Level 2 Data Tree Test XML-------------");
+        System.out.println(v);
+        
+        System.out.println("-----------------------------------------------------");
+       
+        assertTrue(v.contains("<str2>value</str2>"));
+        assertTrue(v.contains("<abc2>0145a3</abc2>"));
+        assertTrue(v.contains("<big_dec>10</big_dec>"));
+        assertTrue(v.contains("<big_dec2>10</big_dec2>"));
+        assertTrue(v.contains("<SubList>Value of SubList</SubList>"));
+        
+        assertTrue(v.contains("<nu3/>"));
+        assertTrue(v.contains("<b2>true</b2>"));
+    }
+    
+    @Test
+    public void ThreeLevelTreeTest() throws IOException {
+    	ValueDataTree.TreeBuilder tb = ValueDataTree.builderOfDataTree();
+    	
+   	 tb.putElement(Key.of("str"), "value");
+        tb.putElement(Key.of("dou"), 4.5d);
+        tb.putElement(Key.of("b"), true);
+        tb.putNull(Key.of("nu"));
+        tb.putElement(Key.of("nu2"), tb.nullElement());
+        tb.putElement(Key.of("big_dec"), BigDecimal.TEN);
+       
+        tb.putElement(Key.of("big_neg"), BigDecimal.valueOf(-1298283729L));
+        tb.putElement(Key.of("bigint"), new BigInteger("2888765432"));
+        byte[] a = { 0x01, 0x45, (byte) 0xa3 };
+        tb.putElement(Key.of("abc"), a);
+        
+        
+       TreeBuilder subTree =  tb.createTree(Key.of("SubTree"));
+       
+       subTree.putElement(Key.of("douSub"), 4.5d);
+       subTree.putElement(Key.of("bSub"), true);
+       subTree.putNull(Key.of("nuSub"));
+       subTree.putElement(Key.of("nu2Sub"), tb.nullElement());
+       subTree.putElement(Key.of("big_decSub"), BigDecimal.TEN);
+      
+       subTree.putElement(Key.of("big_negSub"), BigDecimal.valueOf(-1298283729L));
+       subTree.putElement(Key.of("bigintSub"), new BigInteger("2888765432"));
+       byte[] asubTree = { 0x01, 0x45, (byte) 0xa3 };
+       subTree.putElement(Key.of("abcSub"), a);
+       
+       
+       ValueDataTree.ListBuilder listBuilder = tb.createList(Key.of("SubList"));
+    	
+    	
+    	listBuilder.addElement(new StringElement("Value of SubList"),
+    			new NumberElement(Types.TYPE_INT, 27),
+    			new NumberElement(Types.TYPE_LONG, 9l),
+    			new NumberElement(Types.TYPE_DOUBLE, 9.5d),
+    			new NumberElement(Types.TYPE_DECIMAL, BigDecimal.valueOf(-12312894612L)),
+    			new NumberElement(Types.TYPE_ULONG, new BigInteger("1231259787813"))
+    			);
+    	
+        
+    	ValueDataTree data_tree = tb.build();
+    	
+    	
+    	System.out.println(data_tree);
+    	
+        
+    	
+     	OutputStream outputStream = new ByteArrayOutputStream();
+        data_tree.writeAsJSON(outputStream);
+        
+        JsonReader reader = Json.createReader(new ByteArrayInputStream(
+        		((ByteArrayOutputStream) outputStream).toByteArray())
+        		
+        		);
+        
+        JsonObject jsonObject = reader.readObject();
+        System.out.println("------------Level 3 Data Tree Test JSON-------------");
+        System.out.println(jsonObject);
+        System.out.println("------------------------------------------------");
+        
+        
+        
+        ByteArrayOutputStream bo = new ByteArrayOutputStream(1024);
+        data_tree.writeAsXML("opt",bo);
+        String v = bo.toString(StandardCharsets.UTF_8);
+        System.out.println("------------Level 3 Data Tree Test XML-------------");
+        System.out.println(v);
+        System.out.println("------------------------------------------------");
+       
+        
+        assertTrue(v.contains("<abcSub>0145a3</abcSub>"));
+        assertTrue(v.contains("<big_decSub>10</big_decSub>"));
+       
+        assertTrue(v.contains("<SubList>Value of SubList</SubList>"));
+        assertTrue(v.contains("<SubTree>"));
+        assertTrue(v.contains("</SubTree>"));
+ 
+    }
+    
+    
+    
 }
